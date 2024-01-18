015f4dbb3eeb24cdf3cbd1e3bf021a834787e9a5
kbfleming <156731802+kbfleming@users.noreply.github.com>
2024-01-18 12:57:59 -0500

    Modified both files in cloned repo
    - Deleted a print statement from script1
    - Added a print statement to script2

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print lines from Zen of Python
 print("Beautiful is better than ugly.")
-print("Explicit is better than implicit.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # print other lines from Zen of Python
 print("Simple is better than complex.")
 print("Complex is better than complicated.")
+print("Explicit is better than implicit.")
